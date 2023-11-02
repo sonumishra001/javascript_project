7e198a94bae8139182121ea5b7916ab74e26d649
sonumishra001 <ashutosh.madhipur424@gmail.com>
2023-11-02 17:26:39 +0530

    5 project

--- 5-keyboard/script.js
+++ 5-keyboard/script.js
@@ -1,1 +1,20 @@
-console.log('Project 5');
+const insert = document.getElementById('insert')
+window.addEventListener('keydown', (e)=>{
+    insert.innerHTML = `
+        <div class = 'color'>
+        <table>
+        <tr>
+          <th>Key</th>
+          <th>Keycode</th>
+          <th>Code</th>
+        </tr>
+        <tr>
+          <td>${e.key === " " ? 'space' : e.key}</td>
+          <td>${e.KeyCode}</td>
+          <td>${e.code}</td>
+        </tr>
+      </table>
+    </div>
+    
+    `;
+})
